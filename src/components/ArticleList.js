d3f007fea67bd42edd21b16904188b648e7c2864
Anastasia Shatokhina <shnatya@gmail.com>
2021-11-18 13:29:42 -0800

    commit tree fd736360d12df0a2b54aeec7e18b99a19d19c66b

---
+++ src/components/ArticleList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}){
+    const postList = posts.map(post => <Article key = {post.id} title = {post.title} date = {post.date} preview = {post.preview} />)
+    return (
+        <main>
+            {postList}
+        </main>
+    )
+
+}
+
+export default ArticleList;
